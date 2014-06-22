826db346e06b33709bbc6b8e85a7738602df730f
Takumi Sueda <puhitaku@gmail.com>
2014-06-23 02:25:27 +0900

    wrote main

---
+++ main.py
@@ -0,0 +1,19 @@
+import parser
+import interpreter
+
+def main():
+    import sys
+    script_name = sys.argv[0]
+    file_name = sys.argv[1]
+
+    try:
+        inp = open(script_name, "r")
+    except IndexError:
+        interpreter.run_interactive()
+    except IOError:
+        print(file_name, "cannot be opened.")
+    else:
+        interpreter.run( parser.parse(inp) )
+
+if __name__ == "__main__":
+    main()
